37b9faad87eecf10907fcb742d89975466e62de0
kaibas01 <kaibas01@allegheny.edu>
2023-12-24 06:37:22 -0500

    added todo

--- exercise_one.py
+++ exercise_one.py
@@ -6,11 +6,16 @@ If a string is longer than the other, append the additional letters onto the end
 
 
 def mergeAlternately():
-    word1 = "abc"
-    word2 = "pqr"
-    first = word1[0]
-    sol = word1 + word2 
-    print(f"{sol}\n{first}")
+    word_one = "abc"
+    word_two = "pqr"
+    first_word_one = word_one[0]
+    first_word_two = word_two[0]
+    # TODO: Make this part working 
+    first_word = int(word_one) - int(first_word_one)
+    # TODO: Make this part working as well 
+    second_word = word_two - first_word_two
+    #This is just the output
+    print(first_word + second_word)
     
 
 
