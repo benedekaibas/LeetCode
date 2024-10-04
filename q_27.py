1619bcaa91a56ef245a2c63395a5bb19b3b945b1
benedekaibas <kaibas.benedek02@gmail.com>
2024-10-04 16:13:34 -0400

    changes

--- q_27.py
+++ q_27.py
@@ -6,7 +6,10 @@ def remove_element(nums: List[int], val: int) -> int:
     """Function for implementing remove element method."""
     
     # we should use list comprehension since that's the most efficient way
-    
+    for i in enumerate(nums):
+        if i == val:
+            nums.remove(i) 
+            return nums
         
 if __name__ == "__main__":
     nums = [3,2,2,3]
