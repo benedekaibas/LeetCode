68050b9e9ed5dfdc7303558338ce4e2c583cd296
benedekaibas <kaibas.benedek02@gmail.com>
2024-10-04 12:46:54 -0400

    question 88 completed

--- q_88.py
+++ q_88.py
@@ -1,12 +1,15 @@
 """Merge Sorted Array"""
+from typing import List
 
 def merge_array(list_one: List[int], m: int, list_two: List[int], n: int) -> None:
     """Function for merging arrays."""
     list_one = list_one[:m] + list_two[:n]
-
+    return sorted(list_one)
+    
 
 if __name__ == "__main__":
-    m = 3
-    n = 3
-    list_one = [1,2,3,0,0,0]
-    list_two = [2,5,6]
+    m = 0
+    n = 1
+    list_one = [0]
+    list_two = [1]
+    print(merge_array(list_one, m, list_two, n))
